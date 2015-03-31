946b3c5dca53d9e19d7c3952e7fb74a2165598ad
piwonskp <piwonskp@gmail.com>
2015-03-31 09:25:59 +0000

    Crypt bruteforce

--- cryptbrute.py
+++ cryptbrute.py
@@ -7,7 +7,7 @@ def brute():
     for i in string.ascii_lowercase:
         for j in string.ascii_lowercase:
             for k in string.ascii_lowercase:
-                if crypt.crypt(i+j+k,line[2]) == line:
+                if crypt.crypt(i+j+k,line[1]) == line[1]:
                     print('haslo: ',i+j+k)
     
 brute()
